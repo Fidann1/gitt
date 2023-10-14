1034f8372f42cbaab603a54321d1901f95030a79
fidann <fidannceferli@gmail.com>
2023-10-14 13:54:07 +0400

    array

---
+++ Array.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	int x,v;
+	int array[1000];
+	for(int i=0;i<1000;i++){
+	
+		printf("Please enter number one by one,if you end press v and enter it  \n");
+		scanf("%d",&array[i]);
+		if( array[i]==v)break;
+		x=x+1;
+		
+		
+		
+	}
+
+		
+	
+
+	printf("The lenght of it is %d",x-1);
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
